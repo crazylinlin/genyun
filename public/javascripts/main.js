491b8bd0884aedd980c8470365c678fe68a73a66
crazylinlin <296966596@qq.com>
2017-04-22 23:10:22 +0800

    优化调整
    公用组件，以及公用组件样式优化调整。

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,18 @@
 import $ from "../bower_components/jquery/dist/jquery.min.js";
 import header from "../htm/common/header.html";
+import dialog from "../htm/common/loginAndReg.html";
 /*加载bootstrap样式*/
 require("../bower_components/bootstrap/dist/css/bootstrap.min.css");
 require("../bower_components/bootstrap/dist/css/bootstrap-theme.min.css");
 /*结束*/
 
 $(document).ready(function(){
-	/*添加头部*/
+	/*start 添加头部*/
 	var head = document.getElementById("header");
+	var cur_dialog = document.getElementById("dialog-container");
 	head.innerHTML = header;
+	/*end 添加尾部*/
+	/*start 添加登录以及注册对话框*/
+	cur_dialog.innerHTML = dialog;
+	/*end 添加登陆一集注册对话框*/
 })
