c850df3bd82c1a2e813cd7ccf1b40151057590a3
crazylinlin <296966596@qq.com>
2017-11-12 00:47:40 +0800

    部分界面的优化调整，以及bug修复
    讲师详情界面的信息显示优化，首页课程动画的优化调整。

--- public/javascripts/course_info.js
+++ public/javascripts/course_info.js
@@ -3,5 +3,12 @@ import $ from "../bower_components/jquery/dist/jquery.min.js";
 
 require("../stylesheets/course_info.css");
 $(document).ready(function(){
-	
+	/*讲师的信息全部显示或缩略显示，modified by xuwl：2017/／11/12/00:25*/
+	$(".user-info").hover(function(){
+		var curHeight = $(this).height(),
+			autoHeight = $(this).css('height','auto').height();
+		$(this).height(curHeight).animate({"height":autoHeight},500);
+	},function(){
+		$(this).animate({"height":"120px"});
+	})
 })
